b660f54ff2f9c2c696101910d1e2c9e921cc4d92
NataliaColome <nmcolome@gmail.com>
2019-04-02 23:51:47 -0600

    Setup for deck class and test

---
+++ test/deck_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck.rb'
+require './lib/card.rb'
+
+class DeckTest < Minitest::Test
+  #test it exists
+  #test it has cards
+  #test it counts cards
+  #test it returns cards in category
+end
