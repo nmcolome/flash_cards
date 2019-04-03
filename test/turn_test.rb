a3e69a358b95776aa050c743e28895dde17ce097
NataliaColome <nmcolome@gmail.com>
2019-04-02 22:31:48 -0600

    Add test and functionality for Turn

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,16 +1,38 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn.rb'
+require './lib/card.rb'
 
 class TurnTest < Minitest::Test
 
-  #test it exists with 2 arguments
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
 
-  #test it return a guess
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
 
-  #test it returns a card
+  def test_it_has_a_guess
+    assert_equal "Juneau", @turn.guess
+  end
 
-  #test it's .correct?
+  def test_it_has_a_card
+    assert_equal @card, @turn.card
+  end
 
-  #test it has feedback
+  def test_it_returns_correct_boolean
+    other_answer = Turn.new("Washington", @card)
+
+    assert @turn.correct?
+    refute other_answer.correct?
+  end
+
+  def test_it_has_feedback
+    other_answer = Turn.new("Washington", @card)
+
+    assert_equal "Correct!", @turn.feedback
+    assert_equal "Incorrect.", other_answer.feedback
+  end
 end
