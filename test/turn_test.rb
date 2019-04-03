12b40c5c46c254d60d3b6db2ec7dbf1f5a6735f3
NataliaColome <nmcolome@gmail.com>
2019-04-02 22:14:00 -0600

    Create Turn test setup and files

---
+++ test/turn_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn.rb'
+
+class TurnTest < Minitest::Test
+
+  #test it exists with 2 arguments
+
+  #test it return a guess
+
+  #test it returns a card
+
+  #test it's .correct?
+
+  #test it has feedback
+end
