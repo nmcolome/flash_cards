a9aaf24fe2c1b12601c7d26852ecd37edb546845
NataliaColome <nmcolome@gmail.com>
2019-04-03 01:16:19 -0600

    Create tests for Deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,8 +4,31 @@ require './lib/deck.rb'
 require './lib/card.rb'
 
 class DeckTest < Minitest::Test
-  #test it exists
-  #test it has cards
-  #test it counts cards
-  #test it returns cards in category
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_cards
+    assert_equal @cards, @deck.cards
+    assert_equal @card_1, @deck.cards[0]
+  end
+
+  def test_it_counts_cards
+    assert_equal 3, @deck.count
+  end
+
+  def test_it_returns_cards_in_category
+    assert_equal @card_1, deck.cards_in_category(:Geography)
+    assert_equal [@card_2, @card_3], deck.cards_in_category(:STEM)
+    assert_equal [], deck.cards_in_category("Pop Culture")
+  end
 end
