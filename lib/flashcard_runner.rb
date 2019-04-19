0800d632fcdd14e7ec5cca518a02504e32100665
NataliaColome <nmcolome@gmail.com>
2019-04-18 23:38:01 -0600

    replace hardcoded cards with CardGenerator and cards.txt

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,16 +2,13 @@ require './lib/round.rb'
 require './lib/deck.rb'
 require './lib/card.rb'
 require './lib/turn.rb'
+require './lib/card_generator.rb'
 
 class FlashcardRunner
 
   def initialize
-    @card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-    @card_2 = Card.new("What is Rachel's favorite animal?", "Cat", :"Turing Staff")
-    @card_3 = Card.new("What is Mike's middle name?", "nobody knows", :"Turing Staff")
-    @card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :"Pop Culture")
-
-    @cards = [@card_1, @card_2, @card_3, @card_4]
+    @generator = CardGenerator.new("./lib/cards.txt")
+    @cards = @generator.cards
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
