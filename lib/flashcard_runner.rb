136d739c2ba605311aeba2b47fcf416183389f26
NataliaColome <nmcolome@gmail.com>
2019-04-18 18:56:53 -0600

    Refactor #start for SRP, remove @counter

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -14,26 +14,41 @@ class FlashcardRunner
     @cards = [@card_1, @card_2, @card_3, @card_4]
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
-    @counter = 1
   end
 
-  def run
+  def start
+    welcome_message
+    question_guesser
+    game_over
+  end
+
+  def welcome_message
     puts "Welcome! You're playing with 4 cards.
-  -------------------------------------------------"
-    puts "This is card number #{@counter} out of 4."
-    @cards.each do |card|
-      puts "Question: #{card.question}"
-      user_guess = gets.chomp
-      new_turn = @round.take_turn(user_guess)
-      puts @round.turns[0].feedback
-    end
+-------------------------------------------------"
+  end
+
+  def game_over
     puts "****** Game over! ******"
     puts "You had #{@round.number_correct} correct guesses out of 4 for a total score of #{@round.percent_correct.round}%."
+    categories_score_summary
+  end
+
+  def categories_score_summary
     categories = @cards.map {|x| x.category}.uniq!
     categories.each do |category|
       puts "#{category} - #{@round.percent_correct_by_category(category).round}% correct"
     end
   end
+
+  def question_guesser
+    @cards.each_with_index do |card, index|
+      puts "This is card number #{index + 1} out of 4."
+      puts "Question: #{card.question}"
+      user_guess = gets.chomp
+      new_turn = @round.take_turn(user_guess)
+      puts @round.turns[0].feedback
+    end
+  end
 end
 
-FlashcardRunner.new.run
+FlashcardRunner.new.start
