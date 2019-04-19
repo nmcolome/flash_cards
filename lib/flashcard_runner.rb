549fdab99bac33a636d4cd422ee2f1323222aadc
NataliaColome <nmcolome@gmail.com>
2019-04-18 23:18:11 -0600

    Change categories in hardcoded cards

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,9 +7,9 @@ class FlashcardRunner
 
   def initialize
     @card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-    @card_2 = Card.new("What is Rachel's favorite animal?", "Cat", :Turing_Staff)
-    @card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-    @card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+    @card_2 = Card.new("What is Rachel's favorite animal?", "Cat", :"Turing Staff")
+    @card_3 = Card.new("What is Mike's middle name?", "nobody knows", :"Turing Staff")
+    @card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :"Pop Culture")
 
     @cards = [@card_1, @card_2, @card_3, @card_4]
     @deck = Deck.new(@cards)
