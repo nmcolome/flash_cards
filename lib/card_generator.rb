0405502fada53d7be7c6558ba2cae4f42e21a1e7
NataliaColome <nmcolome@gmail.com>
2019-04-18 23:12:00 -0600

    Read file and create cards - functionality and testing

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class CardGenerator
-  
+  def initialize(filename)
+    @filename = File.new(filename, "r")
+  end
+
+  def cards
+    @filename.map do |line|
+      elements = line.split(",")
+      Card.new(elements[0], elements[1], elements[2].chomp.to_sym)
+    end
+  end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,11 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator.rb'
+require './lib/card.rb'
 
 class CardGeneratorTest < Minitest::Test
-  def test_it_exists
-    generator = CardGenerator.new
 
-    assert_instance_of CardGenerator, generator
+  def setup
+    @filename = "./lib/cards.txt"
+    @generator = CardGenerator.new(@filename)
+  end
+
+  def test_it_exists
+    assert_instance_of CardGenerator, @generator
+  end
+
+  def test_it_has_cards
+    cards = @generator.cards
+
+    assert_equal 4, cards.count
+    assert_instance_of Card, cards[0]
+    assert_equal "What is 5 + 5?", cards[0].question
+    assert_equal "10", cards[0].answer
+    assert_equal :STEM, cards[0].category
   end
 end
